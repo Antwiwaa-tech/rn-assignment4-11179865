1426d5a758678a82b7689392460fa9459c53e161
Antwiwaa <anacondah@st.ug.edu.gh>
2024-06-18 21:39:44 +0000

    Modified App to add Home and Login screen

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Stack from './Navigation/Stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './Login';
+import Home from './Home';
+import { StatusBar } from 'react-native';
 
+const Stack = createNativeStackNavigator();
 
-export default function App() {
-  return <Stack/>
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     
-  
-}
+    </NavigationContainer>
+  );
+};
 
-
+export default App;
