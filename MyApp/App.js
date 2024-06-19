b4f35c9daaf37a75b84e583bf4e829bc5108a611
Antwiwaa <anacondah@st.ug.edu.gh>
2024-06-19 19:44:39 +0000

    Modified content

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import Login from './Login';
 import Home from './Home';
 
 
@@ -10,8 +10,8 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
-       
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
+      <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     
