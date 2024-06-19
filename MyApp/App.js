ddb1e9009e3cae864d466b8442df1d27727cf865
Antwiwaa <anacondah@st.ug.edu.gh>
2024-06-19 09:25:31 +0000

    Modofied App in order to work on Home page

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './Login';
+
 import Home from './Home';
-import { StatusBar } from 'react-native';
+
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+       
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     
